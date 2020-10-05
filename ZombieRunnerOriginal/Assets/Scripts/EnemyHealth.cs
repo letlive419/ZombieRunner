1ecbdbf2878a8a3a21023737791c5b956cecb533
Elvis <letlive419@gmail.com>
2020-10-05 13:06:28 -0400

    added health

---
+++ ZombieRunnerOriginal/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    [SerializeField] float Health = 100f;
+    void Start()
+    {
+        
+    }
+    
+
+    public void DamageTaken(float damage)
+    {
+        Health -= damage;
+        print(Health);
+        if (Health <= 0)
+            {
+            Destroy(gameObject);
+        }
+       
+    }
+}
---
+++ ZombieRunnerOriginal/Assets/Scripts/Weapon.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera myCamera;
+    [SerializeField] float Range = 100f;
+    [SerializeField] public float damage = 10f;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Shoot();
+        }
+
+    }
+    private void Shoot()
+    {
+        RaycastHit Hit;
+        if (Physics.Raycast(myCamera.transform.position, myCamera.transform.forward, out Hit, Range))
+        {
+
+            print(Hit.transform.name);
+            EnemyHealth target = Hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
+            target.DamageTaken(damage);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
