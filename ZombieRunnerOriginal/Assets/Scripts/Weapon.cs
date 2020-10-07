b893d3f7f53d4e238198f1cf4b5716e3b1bb1f4a
Elvis <letlive419@gmail.com>
2020-10-07 13:32:17 -0400

    zoomin and out update

--- ZombieRunnerOriginal/Assets/Scripts/Weapon.cs
+++ ZombieRunnerOriginal/Assets/Scripts/Weapon.cs
@@ -13,7 +13,7 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Fire1"))
+        if (Input.GetButtonDown("Fire1"))
         {
             Shoot();
         }
---
+++ ZombieRunnerOriginal/Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+
+    [SerializeField] Camera cameraFieldOfView;
+
+    float zoomedOutFOV = 60;
+    float zoomedInFOV = 20;
+
+    bool zoomedIn = false;
+    // Start is called before the first frame update
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (zoomedIn == false)
+            {
+                zoomedIn = true;
+                cameraFieldOfView.fieldOfView = zoomedInFOV;
+            }
+            else
+            {
+                zoomedIn = false;
+                cameraFieldOfView.fieldOfView = zoomedOutFOV;
+            }
+        }
+        
+
+    }
+
+   
+}
